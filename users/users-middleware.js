ca6088a9dcdefb124e01d1d3cf1ae5554a85a14a
Jason Maurer <jsonmaur@fastmail.com>
2021-01-12 12:01:10 -0700

    Add some boilerplate code to restrict middleware

--- users/users-middleware.js
+++ users/users-middleware.js
@@ -1,6 +1,15 @@
 function restrict() {
-	// Create a middleware function that restricts routes to authorized users only.
+	// This middleware function should restrict routes to authorized users only.
 	// It should get the username and password from the request headers.
+	return async (req, res, next) => {
+		try {
+			const { username, password } = req.headers
+
+			
+		} catch (err) {
+			next(err)
+		}
+	}
 }
 
 module.exports = {
